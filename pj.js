8fc7e80c1b0af340bae2ce37afc1c32562a07915
Louie Chan <lokyin0831@gmail.com>
2022-08-17 21:58:04 +0800

    add template

--- pj.js
+++ pj.js
@@ -9,12 +9,16 @@ let slider;
 let isGameContinue = true
 let defaultNeighborsOfReproduction = 3
 let defaultNeighborsOfLifeless = 2
+let fr = 1
+let noColor = true
+let isRandomInitialState = false
 
 function setup() {
-    slider = createSlider(2, 10, 2);
+    slider = createSlider(1, 50, 10);
+    slider.style('width', `300px`);
 
     /* Set the canvas to be under the element #canvas*/
-    const canvas = createCanvas(windowWidth, windowHeight - 100);
+    const canvas = createCanvas(windowWidth, windowHeight - 220);
     canvas.parent(document.querySelector('#canvas'));
 
     /*Calculate the number of columns and rows */
@@ -36,28 +40,43 @@ function setup() {
 * Initialize/reset the board state
 */
 function init() {
-    for (let i = 0; i < columns; i++) {
-        for (let j = 0; j < rows; j++) {
-            currentBoard[i][j] = 0;
-            nextBoard[i][j] = 0;
+    if (!isRandomInitialState) {
+        for (let i = 0; i < columns; i++) {
+            for (let j = 0; j < rows; j++) {
+                currentBoard[i][j] = 0;
+                nextBoard[i][j] = 0;
+            }
+        }
+    } else {
+        for (let i = 0; i < columns; i++) {
+            for (let j = 0; j < rows; j++) {
+                currentBoard[i][j] = Math.floor(Math.random() * 2);
+                nextBoard[i][j] = Math.floor(Math.random() * 2);
+            }
         }
     }
 }
 
 function draw() {
-    let fr = slider.value()
+    fr = slider.value()
     frameRate(fr)
+    document.querySelector('.fps')
+        .innerHTML = (`fps: ${fr}`)
 
-    background(255);
+    background(200);
 
 
     generate();
     for (let i = 0; i < columns; i++) {
         for (let j = 0; j < rows; j++) {
-            if (currentBoard[i][j] == 1) {
+            if (currentBoard[i][j] == 1 && nextBoard[i][j] == 1) {
+                fill(80);
+            } else if (currentBoard[i][j] == 1 && noColor) {
                 fill(boxColor);
+            } else if (currentBoard[i][j] == 1 && !noColor) {
+                fill(Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255));
             } else {
-                fill(255);  //can change background boxes color here original:255
+                fill(200);  //can change background boxes color here original:255
             }
             stroke(strokeColor);
             rect(i * unitLength, j * unitLength, unitLength, unitLength);
@@ -150,9 +169,10 @@ document.querySelector('.reset-game')
             .innerHTML = (`If a box has no life and it has ${defaultNeighborsOfReproduction} neighbors, the box in next generation fills with life because of reproduction.`)
         defaultNeighborsOfLifeless = 2
         document.querySelector('.showLonelinessSentence')
-        .innerHTML = (`If a box has life and it has less than ${defaultNeighborsOfLifeless} neighbors. It dies of loneliness. The box becomes lifeless next generation.`)
+            .innerHTML = (`If a box has life and it has less than ${defaultNeighborsOfLifeless} neighbors. It dies of loneliness. The box becomes lifeless next generation.`)
     });
 
+// stop or play switch
 document.querySelector('.stop')
     .addEventListener('click', function () {
         if (isGameContinue) {
@@ -163,6 +183,205 @@ document.querySelector('.stop')
 document.querySelector('.play')
     .addEventListener('click', function () {
         isGameContinue = true
+    });
+
+// random color
+document.querySelector('.color')
+    .addEventListener('click', function () {
+        if (noColor) {
+            noColor = false;
+        } else {
+            noColor = true;
+        }
+    });
+
+// random initial state
+document.querySelector('.random-initial-states')
+    .addEventListener('click', function () {
+        for (let i = 0; i < columns; i++) {
+            for (let j = 0; j < rows; j++) {
+                currentBoard[i][j] = Math.floor(Math.random() * 2);
+                nextBoard[i][j] = Math.floor(Math.random() * 2);
+            }
+        }
+    });
+
+// make a glider
+document.querySelector('.glider')
+    .addEventListener('click', function () {
+        let x = Math.floor(Math.random() * 64) + 1;
+        let y = Math.floor(Math.random() * 34) + 1;
+        currentBoard[x][y] = 1;
+        currentBoard[x][y + 1] = 1;
+        currentBoard[x][y + 2] = 1;
+        currentBoard[x - 1][y + 2] = 1;
+        currentBoard[x - 2][y + 1] = 1;
+    });
+
+// make a Gosper glider gun
+document.querySelector('.gosper-glider-gun')
+    .addEventListener('click', function () {
+
+        currentBoard[1][5] = 1;
+        currentBoard[2][5] = 1;
+        currentBoard[1][6] = 1;
+        currentBoard[2][6] = 1;
+        currentBoard[11][5] = 1;
+        currentBoard[11][6] = 1;
+        currentBoard[11][7] = 1;
+        currentBoard[12][4] = 1;
+        currentBoard[12][8] = 1;
+        currentBoard[13][3] = 1;
+        currentBoard[13][9] = 1;
+        currentBoard[14][3] = 1;
+        currentBoard[14][9] = 1;
+        currentBoard[15][6] = 1;
+        currentBoard[16][4] = 1;
+        currentBoard[16][8] = 1;
+        currentBoard[17][5] = 1;
+        currentBoard[17][6] = 1;
+        currentBoard[17][7] = 1;
+        currentBoard[18][6] = 1;
+        currentBoard[21][3] = 1;
+        currentBoard[21][4] = 1;
+        currentBoard[21][5] = 1;
+        currentBoard[22][3] = 1;
+        currentBoard[22][4] = 1;
+        currentBoard[22][5] = 1;
+        currentBoard[23][2] = 1;
+        currentBoard[23][6] = 1;
+        currentBoard[25][1] = 1;
+        currentBoard[25][2] = 1;
+        currentBoard[25][6] = 1;
+        currentBoard[25][7] = 1;
+        currentBoard[35][3] = 1;
+        currentBoard[35][4] = 1;
+        currentBoard[36][3] = 1;
+        currentBoard[36][4] = 1;
+
+    });
+
+// make a Lightweight spaceship
+document.querySelector('.lightweight-spaceship')
+    .addEventListener('click', function () {
+        y = 5
+        currentBoard[0][4 + y] = 1;
+        currentBoard[0][5 + y] = 1;
+        currentBoard[0][6 + y] = 1;
+        currentBoard[0][12 + y] = 1;
+        currentBoard[0][13 + y] = 1;
+        currentBoard[0][14 + y] = 1;
+        currentBoard[1][4 + y] = 1;
+        currentBoard[1][7 + y] = 1;
+        currentBoard[1][11 + y] = 1;
+        currentBoard[1][14 + y] = 1;
+        currentBoard[2][4 + y] = 1;
+        currentBoard[2][14 + y] = 1;
+        currentBoard[3][4 + y] = 1;
+        currentBoard[3][14 + y] = 1;
+        currentBoard[4][5 + y] = 1;
+        currentBoard[4][7 + y] = 1;
+        currentBoard[4][11 + y] = 1;
+        currentBoard[4][13 + y] = 1;
+        currentBoard[6][8 + y] = 1;
+        currentBoard[6][9 + y] = 1;
+        currentBoard[6][10 + y] = 1;
+        currentBoard[7][7 + y] = 1;
+        currentBoard[7][11 + y] = 1;
+        currentBoard[8][7 + y] = 1;
+        currentBoard[8][11 + y] = 1;
+        currentBoard[9][8 + y] = 1;
+        currentBoard[9][10 + y] = 1;
+        currentBoard[10][5 + y] = 1;
+        currentBoard[10][6 + y] = 1;
+        currentBoard[10][12 + y] = 1;
+        currentBoard[10][13 + y] = 1;
+        currentBoard[11][5 + y] = 1;
+        currentBoard[11][6 + y] = 1;
+        currentBoard[11][7 + y] = 1;
+        currentBoard[11][8 + y] = 1;
+        currentBoard[11][9 + y] = 1;
+        currentBoard[11][10 + y] = 1;
+        currentBoard[11][11 + y] = 1;
+        currentBoard[11][12 + y] = 1;
+        currentBoard[11][13 + y] = 1;
+        currentBoard[12][4 + y] = 1;
+        currentBoard[12][8 + y] = 1;
+        currentBoard[12][9 + y] = 1;
+        currentBoard[12][10 + y] = 1;
+        currentBoard[12][14 + y] = 1;
+        currentBoard[13][2 + y] = 1;
+        currentBoard[13][3 + y] = 1;
+        currentBoard[13][5 + y] = 1;
+        currentBoard[13][6 + y] = 1;
+        currentBoard[13][12 + y] = 1;
+        currentBoard[13][13 + y] = 1;
+        currentBoard[13][15 + y] = 1;
+        currentBoard[13][16 + y] = 1;
+        currentBoard[14][5 + y] = 1;
+        currentBoard[14][13 + y] = 1;
+        currentBoard[15][6 + y] = 1;
+        currentBoard[15][7 + y] = 1;
+        currentBoard[15][11 + y] = 1;
+        currentBoard[15][12 + y] = 1;
+        currentBoard[16][2 + y] = 1;
+        currentBoard[16][4 + y] = 1;
+        currentBoard[16][5 + y] = 1;
+        currentBoard[16][7 + y] = 1;
+        currentBoard[16][11 + y] = 1;
+        currentBoard[16][13 + y] = 1;
+        currentBoard[16][14 + y] = 1;
+        currentBoard[16][16 + y] = 1;
+        currentBoard[17][6 + y] = 1;
+        currentBoard[17][7 + y] = 1;
+        currentBoard[17][11 + y] = 1;
+        currentBoard[17][12 + y] = 1;
+        currentBoard[18][1 + y] = 1;
+        currentBoard[18][2 + y] = 1;
+        currentBoard[18][4 + y] = 1;
+        currentBoard[18][5 + y] = 1;
+        currentBoard[18][13 + y] = 1;
+        currentBoard[18][14 + y] = 1;
+        currentBoard[18][16 + y] = 1;
+        currentBoard[18][17 + y] = 1;
+        currentBoard[19][1 + y] = 1;
+        currentBoard[19][2 + y] = 1;
+        currentBoard[19][4 + y] = 1;
+        currentBoard[19][14 + y] = 1;
+        currentBoard[19][16 + y] = 1;
+        currentBoard[19][17 + y] = 1;
+        currentBoard[20][1 + y] = 1;
+        currentBoard[20][5 + y] = 1;
+        currentBoard[20][6 + y] = 1;
+        currentBoard[20][7 + y] = 1;
+        currentBoard[20][11 + y] = 1;
+        currentBoard[20][12 + y] = 1;
+        currentBoard[20][13 + y] = 1;
+        currentBoard[20][17 + y] = 1;
+        currentBoard[21][0 + y] = 1;
+        currentBoard[21][1 + y] = 1;
+        currentBoard[21][5 + y] = 1;
+        currentBoard[21][6 + y] = 1;
+        currentBoard[21][12 + y] = 1;
+        currentBoard[21][13 + y] = 1;
+        currentBoard[21][17 + y] = 1;
+        currentBoard[21][18 + y] = 1;
+        currentBoard[22][5 + y] = 1;
+        currentBoard[22][6 + y] = 1;
+        currentBoard[22][7 + y] = 1;
+        currentBoard[22][11 + y] = 1;
+        currentBoard[22][12 + y] = 1;
+        currentBoard[22][13 + y] = 1;
+        currentBoard[23][5 + y] = 1;
+        currentBoard[23][6 + y] = 1;
+        currentBoard[23][7 + y] = 1;
+        currentBoard[23][11 + y] = 1;
+        currentBoard[23][12 + y] = 1;
+        currentBoard[23][13 + y] = 1;
+        currentBoard[24][5 + y] = 1;
+        currentBoard[24][6 + y] = 1;
+        currentBoard[24][12 + y] = 1;
+        currentBoard[24][13 + y] = 1;
     });
 
 
